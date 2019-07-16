fa4175d81cc78cc0f07d28e78553f9b0f7da0a7e
lijt <lijt@eetrust.com>
2019-07-16 15:24:43 +0800

    2019.7.16

---
+++ app/src/main/java/com/example/presentation/di/ActivityScope.kt
@@ -0,0 +1,11 @@
+package com.example.presentation.di
+
+import javax.inject.Scope
+
+/**
+ *  Author : JinTao Li
+ *  Create Time : 2019/7/10
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
--- app/src/main/java/com/example/presentation/di/modules/ActivityModules.kt
+++ app/src/main/java/com/example/presentation/di/modules/ActivityModules.kt
@@ -1,6 +1,7 @@
 package com.example.presentation.di.modules
 
 import android.app.Activity
+import com.example.presentation.di.ActivityScope
 import dagger.Module
 import dagger.Provides
 
@@ -8,10 +9,12 @@ import dagger.Provides
  * Created by : JinTao Li
  * Created time : 2019/4/19
  */
+
 @Module
-class ActivityModules {
+class ActivityModules(val activity: Activity) {
     @Provides
+    @ActivityScope
     fun activity(): Activity {
-        return activity()
+        return this.activity
     }
 }
